60e26f13e8e5ee95bd137cdaa853d254da1722b3
Andie <andyespino07@hotmail.com>
2025-09-22 12:41:37 -0600

    Se añade conexión simple a SQL

---
+++ server/config/connection.js
@@ -0,0 +1,36 @@
+// Conexion a la base de datos
+const mysql = require("mysql2")
+
+// Configuración de la conexión
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "sga",
+})
+
+// Control de errores
+connection.connect((error) => {
+  if (error) {
+    console.error("Error al conectar a la base de datos: " + error.stack)
+    return
+  }
+  console.log("Conexión exitosa a la base de datos MySQL. \n")
+})
+module.exports = connection
+
+// Ejemplo de uso de la conexión
+const query = "SELECT * from users"
+
+connection.query(query, (error, results) => {
+  if (error) {
+    console.error("Error al ejecutar la consulta: " + error.stack)
+    return
+  }
+  console.log("Query solicitado:" + query + "\n")
+  console.log("Resultados de la consulta:")
+  console.log(results)
+})
+
+// Cerrar la conexión
+connection.end()
